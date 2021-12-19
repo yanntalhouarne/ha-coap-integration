0d2d52a9c0840fef9a7899e7563c1f902925cd5e
Yann Talhouarne <talhouarne.yann@gmail.com>
2021-12-19 14:59:52 -0600

    Add files via upload

---
+++ sensor.py
@@ -0,0 +1,153 @@
+"""myCoap interface."""
+from datetime import timedelta
+import logging
+
+# Bring in CoAP
+from aiocoap import *
+
+import voluptuous as vol
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_FRIENDLY_NAME,
+    CONF_SCAN_INTERVAL,
+    TEMP_CELSIUS,
+)
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.event import async_track_time_interval
+
+_LOGGER = logging.getLogger(__name__)
+
+CONST_DEFAULT_SCAN_PERIOD_S = 60.0
+
+CONST_COAP_PROTOCOL = "coap://"
+CONST_COAP_STRING_TRUE = "1"
+CONST_COAP_STRING_FALSE = "0"
+
+# protocol = ""
+
+# for data validation
+PLATFORM_SCHEMA = vol.All(
+    PLATFORM_SCHEMA.extend(
+        {
+            vol.Required(CONF_HOST): cv.string,
+            vol.Required(CONF_FRIENDLY_NAME): cv.string,
+            vol.Optional(CONF_SCAN_INTERVAL): cv.string,
+        },
+        extra=vol.PREVENT_EXTRA,
+    )
+)
+
+# setup platform
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up a temperature sensor."""
+
+    print("Set up the temperature sensor")
+
+    host = config.get(CONF_HOST)
+
+    # Use all sensors by default
+    hass_sensors = []
+
+    # Setup Async CoAP
+    protocol = await Context.create_client_context()
+
+    # Add sensors
+    hass_sensors.append(
+        CoAPsensorNode(
+            host,
+            "temperature",
+            protocol,
+            config.get(CONF_FRIENDLY_NAME) + " - Temperature",
+            TEMP_CELSIUS,
+            1,
+        )
+    )
+
+    print("Adding temperature sensor done")
+
+    async_add_entities(hass_sensors)
+
+    print("async_add_entities done")
+
+    async def async_update_sensors(event):
+        """Update temperature sensor."""
+
+        # Update sensors based on scan_period set below which comes in from the config
+        for sensor in hass_sensors:
+            await sensor.async_update_values()
+
+    # update sensor every 5 seconds
+    async_track_time_interval(hass, async_update_sensors, timedelta(seconds=1))
+
+
+class CoAPsensorNode(Entity):
+    """Representation of a CoAP sensor node."""
+
+    def __init__(self, host, uri, protocol, name, unit, round_places):
+        """Initialize the sensor."""
+
+        print("Init Sensor " + uri)
+
+        self._uri = uri
+        self._name = name
+        self._unit = unit
+        self._round_places = round_places
+        self._state = "0"
+        self._host = host
+        self._protocol = protocol
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit the value is expressed in."""
+        return self._unit
+
+    @property
+    def poll(self):
+        """Sensors are polled."""
+        return True
+
+    @callback
+    async def async_update_values(self):
+        """Update this sensor."""
+        try:
+            # print("Update2: " + self._uri)
+            # _LOGGER.info("Update: " + self._uri)
+            request = Message(code=GET)
+            _uri = CONST_COAP_PROTOCOL + self._host + "/" + self._uri
+            request.set_request_uri(uri=_uri)
+            response = await self._protocol.request(request).response
+            #print(response.payload)
+	    # Check for change
+            if self._state != round(float(int(response.payload, 10)), self._round_places):
+                # Round result to make the ui look nice
+                self._state = round(float(int(response.payload, 10)), self._round_places)
+                _LOGGER.info(
+                    "Nucleo %s changed: %s - %r" % (self._uri, response.code, self._state)
+                )
+                self.async_write_ha_state()
+            else:
+                _LOGGER.info(
+                    "%s no change... current value = %s" % (self._uri, self._state)
+                )
+        except Exception as e:
+            _LOGGER.info("Exception - Failed to GET resource: " + self._uri)
+            _LOGGER.info(e)
+
+    @property
+    def unique_id(self):
+        """Return a unique identifier for this sensor."""
+        return f"{self._name}"
---
+++ switch.py
@@ -0,0 +1,157 @@
+"""LEMA Off-Grid interface."""
+from datetime import timedelta
+import logging
+import asyncio
+
+# Bring in CoAP
+from aiocoap import *
+
+import voluptuous as vol
+
+from homeassistant.components.switch import PLATFORM_SCHEMA
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_FRIENDLY_NAME,
+    CONF_SCAN_INTERVAL,
+)
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import ToggleEntity
+from homeassistant.helpers.event import async_track_time_interval
+
+_LOGGER = logging.getLogger(__name__)
+
+CONST_DEFAULT_SCAN_PERIOD_S = 60
+
+# TODO figure out why entity_playform.py has self.scan_interval set to a string "7"
+# ...seconds.  It's used once on boot but then our switch async_setup_platform
+# completes and the exception never happens again.
+# Seems like a race condition on init or we neet to setup something else that
+# we are not doing.  See how platform is used in zwave.py
+# must have to do with vol.Optional(CONF_SCAN_INTERVAL): cv.string
+SCAN_INTERVAL = timedelta(seconds=CONST_DEFAULT_SCAN_PERIOD_S)
+
+CONST_COAP_PROTOCOL = "coap://"
+CONST_COAP_STRING_TRUE = "1"
+CONST_COAP_STRING_FALSE = "0"
+
+protocol = ""
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_FRIENDLY_NAME): cv.string,
+        vol.Optional(CONF_SCAN_INTERVAL): cv.string
+    }
+)
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up coap Switches """
+
+    host = config.get(CONF_HOST)
+
+    # Use all switches by default
+    hass_switches = []
+
+    # Setup Async CoAP
+    protocol = await Context.create_client_context()
+
+    # Add switches
+    hass_switches.append(coap_Switch(host, "switch", protocol, config.get(CONF_FRIENDLY_NAME), False, None))
+
+    # Add the entities
+    async_add_entities(hass_switches)
+
+    async def async_update_switches(event):
+        """Update all the coap switches."""
+
+        # Update sensors based on scan_period set below which comes in from the config
+        for sw in hass_switches:
+            await sw.async_update_values()
+    
+    async_track_time_interval(hass, async_update_switches, timedelta(seconds=1))
+
+class coap_Switch(ToggleEntity):
+    """Representation of a Digital Output."""
+
+    def __init__(self, host, uri, protocol, name, unit, invert_logic):
+        """Initialize the pin."""
+
+        print("Init coap switch = " + uri)
+
+        self._host = host
+        self._uri = uri
+        self._name = name
+        self._unit = unit
+        self._invert_logic = invert_logic
+        self._state = False
+        self._protocol = protocol
+        self.async_turn_off()
+
+    @property
+    def name(self):
+        """Return the name of the switch."""
+        return self._name
+
+    @property
+    def should_poll(self):
+        """Return the polling state."""
+        return True
+
+    @property
+    def is_on(self):
+        """Return true if device is on."""
+        return self._state
+
+    async def async_turn_on(self, **kwargs):
+        """Turn the device on."""
+        try:
+            _LOGGER.info("HA calling TURN_ON for " + self._uri)
+            request = Message(code=PUT, payload=CONST_COAP_STRING_TRUE.encode("ascii"), uri=CONST_COAP_PROTOCOL + self._host + "/" + self._uri)
+            response = await self._protocol.request(request).response
+            self._state = True
+            self.schedule_update_ha_state()
+        except Exception as e:
+            _LOGGER.info("Failed to PUT resource: " + self._uri)
+            _LOGGER.info(e)
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the device off."""
+        try:
+            _LOGGER.info("HA calling TURN_OFF for " + self._uri)
+            request = Message(code=PUT, payload=CONST_COAP_STRING_FALSE.encode("ascii"), uri=CONST_COAP_PROTOCOL + self._host + "/" + self._uri)
+            response = await self._protocol.request(request).response
+            self._state = False
+            self.schedule_update_ha_state()
+        except Exception as e:
+            _LOGGER.info("Failed to PUT resource: " + self._uri)
+            _LOGGER.info(e)
+
+    @callback
+    async def async_update_values(self):
+        """Update this switch."""
+        try:
+            request = Message(code=GET, uri=CONST_COAP_PROTOCOL + self._host + "/" + self._uri)
+            response = await self._protocol.request(request).response
+            print("Payload received is: %s" % (response.payload))
+            response_bool = False
+            if (response.payload == b'\x01'): # TODO: make this a character rather than boolean
+                response_bool = True
+
+            # Check for change
+            if (self._state != response_bool):
+                self._state = response_bool
+                _LOGGER.info("%s changed: %s - %s" % (self._uri, response.code, str(response_bool)))
+                self.async_write_ha_state()
+            else:
+                _LOGGER.info("%s no change..." % (self._uri))
+
+        except Exception as e:
+            _LOGGER.info("Failed to GET resource: " + self._uri)
+            _LOGGER.info(e)
+
+    @property
+    def unique_id(self):
+        """Return a unique identifier for this sensor."""
+        return f"{self._name}"
